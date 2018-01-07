96b2ad6180edb8477113fd90caff1df7d8ce2966
Kunal Sharma <kunalsharma.ks13@gmail.com>
2018-01-07 17:18:04 +0530

    initial Commit

---
+++ Assets/Scripts/ObjectLoader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ObjectLoader : MonoBehaviour
+{
+
+    public GameObject loadingScreen;
+    public Slider slider;
+
+
+    public void LoadLevel(int SceneIndex)
+    {
+        StartCoroutine(loadAsynchronously(SceneIndex));
+    }
+
+    IEnumerator loadAsynchronously(int SceneIndex)
+    {
+
+        AsyncOperation operation = SceneManager.LoadSceneAsync(SceneIndex);
+        loadingScreen.SetActive(true);
+        while (!operation.isDone)
+        {
+            float prog = Mathf.Clamp01(operation.progress / 0.9f);
+            Debug.Log(prog);
+            slider.value = prog;
+            yield return null;
+        }
+    }
+}
